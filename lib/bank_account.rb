4c162960fdb24b8a428a45a3fd0421033d82c52a
Erin Green <erin.y.green@gmail.com>
2018-10-11 00:34:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,17 +1,32 @@
 class BankAccount
   
-  attr_accessor :balance, :status 
+  attr_accessor :balance, :status, :all 
   attr_reader :name 
+  
+  @@all = []
 
 def initialize(name)
   @name = name
   @balance = 1000
   @status = "open"
+  @@all << self 
 end
 
-def balance
+def display_balance
   @balance
 end
+
+def deposit
+  
+end
+
+def valid?
+end
+
+def close_account
+end
+
+
 
 
 end
